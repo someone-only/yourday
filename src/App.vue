870cd2d2a3b153bff5ae38e2f682d92e1ec3e656
someone-only <akumemailanda@gmail.com>
2023-10-01 21:41:29 +0700

    initial init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import particle from "./components/particle.vue";
+</script>
+
+<template>
+  <particle />
+  <div>
+    <h1>Happy Birthday</h1>
+    <p>FITRI DWI ZAHARI</p>
+  </div>
+  <div class="container">
+    <p>
+      Semoga panjang umur, Sehat selalu, Bisa menjadi pribadi yang lebih baik,
+      Makin dewasa, Dan semoga apa yang kamu inginkan semoga terwujud
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 4.2em;
+  line-height: 1.1;
+  font-weight: bold;
+}
+p {
+  font-size: 1.5em;
+}
+.container {
+  margin-top: 4rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+</style>
---
+++ src/components/particle.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { loadFull } from "tsparticles";
+import type { Engine } from "tsparticles-engine";
+
+const particlesInit = async (engine: Engine) => {
+  //await loadFull(engine);
+  await loadFull(engine);
+};
+
+
+</script>
+
+<template>
+  <vue-particles
+    id="tsparticles"
+    :particlesInit="particlesInit"
+    :options="{
+      background: {
+        color: {
+          value: '#fff',
+        },
+      },
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      fpsLimit: 30,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: false,
+            mode: 'push',
+          },
+          onHover: {
+            enable: false,
+            mode: 'repulse',
+          },
+          resize: true,
+        },
+        modes: {
+          bubble: {
+            distance: 400,
+            duration: 2,
+            opacity: 0.8,
+            size: 40,
+          },
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: 'random',
+        },
+        links: {
+          color: '#ffffff',
+          distance: 150,
+          enable: false,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outMode: 'bounce',
+          random: false,
+          speed: 4,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: 'circle',
+        },
+        size: {
+          random: true,
+          value: 5,
+        },
+      },
+      detectRetina: true,
+    }"
+  />
+</template>
